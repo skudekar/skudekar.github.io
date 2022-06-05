fe0e1f7457966cd70907134c166df0941f61cec2
tool2fit <61243076+tool2fit@users.noreply.github.com>
2022-06-05 16:21:54 -0400

    new videos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", startup, false);
 var numOfVideo = 0
-let list_of_videos = ["test_cap-and-tony_oklabal_adap0_modDepth=0.0625_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=12.mp4", "test_james-bond-macau-dark_oklabal_adap4_modDepth=0.06_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=12.mp4", "test_stranger-things-fashion_oklabal_adap0_modDepth=0.0525_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=12.mp4", "test_rocky-steps-cropped_oklabal_adap0_modDepth=0.0625_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=12.mp4", "test_big-buck-long-clip_oklabal_adap0_modDepth=0.0625_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=12.mp4"]
+let list_of_videos = ["test_cap-and-tony_oklabal_adap0_modDepth=0.065_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4", "test_james-bond-macau-dark_oklabal_adap4_modDepth=0.06_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4", "test_rocky-steps-cropped_oklabal_adap0_modDepth=0.065_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4", "test_big-buck-long-clip_oklabal_adap0_modDepth=0.065_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4", "test_shawshank-ending_oklabal_adap0_modDepth=0.065_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4", "test_harry-met-sally-cropped_oklabal_adap0_modDepth=0.07_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4", "test_ff_oklabal_adap4_modDepth=0.065_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4", "test_stranger-things_oklabal_adap0_modDepth=0.08_minLval=0.3_minModVal=0.025_redByVal=0.02_screenBorderPixels=17_frameInterpolation=linear.mp4"]
 let screenWidth = window.screen.width * window.devicePixelRatio // send this to Python server for decoder processing; this is the 'width' of the screen
 let screenHeight = window.screen.height * window.devicePixelRatio // send this to Python server for decoder processing; this is the 'height' of the screen
 
@@ -9,10 +9,11 @@ function startup() {
  
   // Get the reference to video
   const videoSection = document.getElementById("videoSection");
-
-  videoSection.style.display = "none";
+  const videoAndTextSection = document.getElementById("videoAndTextSection");
+  videoAndTextSection.style.display = "none";
 
   // On pressing ENTER call toggleFullScreen method
+  const video = document.getElementById("video");  
   document.addEventListener("keypress", function(e) {
     if (e.key === 'Enter') {
       toggleFullScreen(video);
@@ -22,6 +23,8 @@ function startup() {
 
 function playVideo() {    
   console.log("Inside playVideo:= ", numOfVideo)
+  const videoAndTextSection = document.getElementById("videoAndTextSection");
+  videoAndTextSection.style.display = "block";
   const videoSection = document.getElementById("videoSection");
   const runVideoButton = document.getElementById("runVideoButton");
   videoSection.style.display = "block";
@@ -45,14 +48,18 @@ function playVideo() {
 }
 
 function toggleFullScreen(video) {
+  const videoPlayer = document.getElementById("video");  
   if (!document.fullscreenElement) {
     // If the document is not in full screen mode
     // make the video full screen
     video.requestFullscreen();
+    //videoPlayer.style.width = 'auto'
+    //videoPlayer.style.height = '100%'
   } else {
     // Otherwise exit the full screen
     if (document.exitFullscreen) {
       document.exitFullscreen();
+      //videoPlayer.style.width = '480px'      
     }
   }
 }
