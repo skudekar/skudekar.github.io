4907ce0ebe76a9167b8b9ef4d4d9266fba8320c0
tool2fit <61243076+tool2fit@users.noreply.github.com>
2022-06-28 11:03:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ let screenWidth = window.screen.width * window.devicePixelRatio // send this to 
 let screenHeight = window.screen.height * window.devicePixelRatio // send this to Python server for decoder processing; this is the 'height' of the screen
 
 function startup() {
-  
+ 
+  // check screen FPS
+  calcFPS({count: 30})
  
   // Get the reference to video
   const videoSection = document.getElementById("videoSection");
@@ -174,4 +176,28 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+// check if the screen has 60Hz refresh rate
+function calcFPS(opts){
+  var requestFrame = window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame;
+  if (!requestFrame) return true; // Check if "true" is returned; 
+                                  // pick default FPS, show error, etc...
+  function checker(){
+      if (index--) requestFrame(checker);
+      else {
+          // var result = 3*Math.round(count*1000/3/(performance.now()-start));
+          var result = count*1000/(performance.now()- start);
+          if (typeof opts.callback === "function") opts.callback(result);
+          console.log("Calculated: "+result+" frames per second");
+          if (Math.round(result) <= 50) {
+            alert('Your screen refresh rate is ' + Math.round(result) + "Hz. Encoded videos will not be perfectly displayed. Please use a screen with 60Hz refresh rate.")
+          }
+      }
+  }
+  if (!opts) opts = {};
+  var count = opts.count||60, index = count, start = performance.now();
+  checker();
 }
