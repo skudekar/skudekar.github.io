a206cb4eaf9e13a25aad69fe03fe03aebd918608
tool2fit <61243076+tool2fit@users.noreply.github.com>
2022-09-06 11:37:23 -0400

    added dior video

--- assets/videos/genTwoMinuteVideos.py
+++ assets/videos/genTwoMinuteVideos.py
@@ -8,19 +8,21 @@ Created on Thu Jul 14 14:22:31 2022
 import os
 from os import listdir
 import cv2
+import sys
 
 timeRequired = 60 # two minute long movie
 
-for f in listdir():
-    if f.startswith('test_'):
-        print(f)
-        cap = cv2.VideoCapture(f)
-        fps = cap.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
-        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        duration = frame_count/fps
-        numOfRepetitions = int(timeRequired/duration)
-        filemylist = open("mylist.txt", "w")
-        for i in range(0,numOfRepetitions):
-            filemylist.write('file %s\n' % f)
-        filemylist.close()        
-        os.system("ffmpeg -f concat -safe 0 -i mylist.txt -c copy " + os.path.splitext(f)[0] + "_LONG.mp4")
+# for f in listdir():
+#     if f.startswith('test_'):
+f = sys.argv[1] # filename of the mp4 we wish to concatenate
+print(f)
+cap = cv2.VideoCapture(f)
+fps = cap.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+duration = frame_count/fps
+numOfRepetitions = int(timeRequired/duration)
+filemylist = open("mylist.txt", "w")
+for i in range(0,numOfRepetitions):
+    filemylist.write('file %s\n' % f)
+filemylist.close()        
+os.system("ffmpeg -f concat -safe 0 -i mylist.txt -c copy " + os.path.splitext(f)[0] + "_LONG.mp4")
